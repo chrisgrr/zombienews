a3e3b007e755d18d4ea79e5a21f7bf0649dbc336
chrisgrr <chrisgrr@gmail.com>
2014-07-25 18:42:07 +0100

    add twitter API integration

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+$TWITTER = Twitter::REST::Client.new do |config|
+  config.consumer_key		 = "g0eznujrRIC9BMz0uYY0B4IWP"
+  config.consumer_secret	 = "grQpN9gbtT1ABxecNp9VStre0aSbyLaoBDzYM2JAx8XPtxhMBA"
+  config.access_token        = "2662308056-3FrOeBBezj9W5h5pNyFteMkeXmAqkWTOMmFYKjP"
+  config.access_token_secret = "k2IxB90vq6Xmmjy9xpQrI2o6MnKfPAjWOYhjSOJNRBt6Y"
+end
--- spec/requests/zombie_spec.rb
+++ spec/requests/zombie_spec.rb
@@ -1,20 +1,46 @@
 require 'rails_helper'
 
-describe "Home Page" do
+describe "Zombie Pages" do
 
-  describe "GET Home Page" do
+  describe "Home Page" do
 
-    it "should load the page" do
-      get root_path
-      response.status.should be(200)
+	describe "GET Home Page" do
+
+	  it "should load the page" do
+	    get root_path
+	    response.status.should be(200)
+	  end
+	end
+
+    describe "Content" do
+
+      before { visit root_path }
+
+  	  it "should have the content Zombie News" do
+  	    expect(page).to have_content("Zombie News")
+  	  end
     end
+
+    describe "Tweets" do
+
+ 	  it "should return an array" do
+ 	  	tweet = $TWITTER.search("#zombies", :result_type => "recent").take(20)
+ 	  	expect(tweet).to be_a Array
+ 	  end
+ 	end
   end
 
-  describe "Content" do
+  describe "About Page" do
 
-  	it "should have the title Zombie News" do
-  	  visit root_path
-  	  expect(page).to have_content("Zombie News")
+  	describe "Content" do
+
+  	  before { visit about_path }
+
+  	  it "should have the content About Zombie News" do
+		expect(page).to have_content("About Zombie News")
+  	  end
   	end
   end
 end
+
+#Testing tweets tweets.should respond_to(methods) - I think this is the way to go?
