fd23440664f4b67a1d231f7c15337d2601c26a9c
chrisgrr <chrisgrr@gmail.com>
2014-07-26 11:50:32 +0100

    add fragment caching for tweets

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module Znews
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # config.i18n.default_locale = :default
+    config.cache_store = :memory_store
   end
 end
